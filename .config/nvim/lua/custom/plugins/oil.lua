478a75c689b5f0cd8607c3fed419bb57f6170966
Janus Hannesarson <janushannesarson@gmail.com>
2024-03-26 23:36:13 +0100

    awesome oil keybinding (<C-e>)

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -2,29 +2,19 @@ return {
 	'stevearc/oil.nvim',
 	opts = {
 		default_file_explorer = true,
-		float = {
-			-- Padding around the floating window
-			padding = 2,
-			max_width = 0,
-			max_height = 0,
-			border = "rounded",
-			win_options = {
-				winblend = 0,
-			},
-			-- This is the config that will be passed to nvim_open_win.
-			-- Change values here to customize the layout
-			override = function(conf)
-				return conf
-			end,
-		},
 		win_options = {
 			signcolumn = "number",
-		}
+		},
+		keymaps = {
+			["<C-t>"] = false,
+			["<C-e>"] = "actions.close",
+			["<C-c>"] = false,
+		},
 	},
 	-- Optional dependencies
 	keys =
 	{
-		{ "<Leader>e", "<cmd>lua require('oil').toggle_float('.')<CR><cmd>lua require('oil').discard_all_changes()<CR>", desc = "Oil" },
+		{ "<C-e>", "<cmd>Oil<CR><cmd>lua require('oil').discard_all_changes()<CR>", desc = "Oil" },
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	lazy = false
