84d34d636d89d8014b6b5a1c89fd42eb6e53b25e
Janus Hannesarson <janushannesarson@gmail.com>
2024-04-02 00:21:46 +0200

    which key no lag

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -110,9 +110,14 @@ require('lazy').setup({
 			'rafamadriz/friendly-snippets',
 		},
 	},
-
-	-- Useful plugin to show you pending keybinds.
-	{ 'folke/which-key.nvim',  opts = {} },
+	{
+		'folke/which-key.nvim',
+		event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+		config = function() -- This is the function that runs, AFTER loading
+			require('which-key').setup()
+			vim.opt.timeoutlen = 0
+		end,
+	},
 	{
 		-- Adds git related signs to the gutter, as well as utilities for managing changes
 		'lewis6991/gitsigns.nvim',
